cf704156bf313e909135b691bac6194be5b89e89
Marc <marcjfj@gmail.com>
2020-02-25 22:10:24 -0600

    added build step

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PODCHAT' });
 });
 router.get('/app', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 module.exports = router;
