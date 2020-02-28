49044f05a643fa55e2dcdad03001571046180053
Brett Porter <gkdesign06@gmail.com>
2020-02-27 21:30:26 -0600

    updated the validation

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,7 +20,7 @@ const Header = () => {
       gsap.to('#start', 0.4, { morphSVG: '#start' });
     }
   };
-    
+
   useEffect(() => {
     user.checkUser();
   }, []);
@@ -28,32 +28,42 @@ const Header = () => {
   return (
     <Fragment>
       {openMenu ? <Backdrop onClick={() => handleMenu()} /> : null}
-      {openMenu && <DropMenu handleMenu={() => handleMenu()} setOpen={() => setOpenMenu()} onClick={() => handleMenu()} />}
+      {openMenu && (
+        <DropMenu
+          handleMenu={() => handleMenu()}
+          setOpen={() => setOpenMenu()}
+          onClick={() => handleMenu()}
+        />
+      )}
       <header>
-        <button className='menu-btn' onClick={() => handleMenu()}>
-          <svg
-            version='1.1'
-            xmlns='http://www.w3.org/2000/svg'
-            x='0px'
-            y='0px'
-            viewBox='0 0 24 24'
-          >
-            <g>
-              <polygon
-                id='end'
-                className='st0'
-                points='18.6,6.8 17.1,5.3 12,10.5 6.8,5.3 5.3,6.8 10.5,11.9 5.3,17.1 6.8,18.6 12,13.4 17.1,18.6 18.6,17.1 13.4,11.9'
-              />
-            </g>
-            <g>
-              <path
-                id='start'
-                className='st0'
-                d='M3.2,11h13v2.2h-13V11z M3.2,5.5h17.4v2.2H3.2V5.5z M3.2,18.6h7.9v-2.2H3.2V18.6z'
-              />
-            </g>
-          </svg>
-        </button>
+        {user.id !== '' ? (
+          <button className='menu-btn' onClick={() => handleMenu()}>
+            <svg
+              version='1.1'
+              xmlns='http://www.w3.org/2000/svg'
+              x='0px'
+              y='0px'
+              viewBox='0 0 24 24'
+            >
+              <g>
+                <polygon
+                  id='end'
+                  className='st0'
+                  points='18.6,6.8 17.1,5.3 12,10.5 6.8,5.3 5.3,6.8 10.5,11.9 5.3,17.1 6.8,18.6 12,13.4 17.1,18.6 18.6,17.1 13.4,11.9'
+                />
+              </g>
+              <g>
+                <path
+                  id='start'
+                  className='st0'
+                  d='M3.2,11h13v2.2h-13V11z M3.2,5.5h17.4v2.2H3.2V5.5z M3.2,18.6h7.9v-2.2H3.2V18.6z'
+                />
+              </g>
+            </svg>
+          </button>
+        ) : (
+          <button className='menu-btn'></button>
+        )}
         <Link to={'/'}>
           <h1>PODCHAT</h1>
         </Link>
